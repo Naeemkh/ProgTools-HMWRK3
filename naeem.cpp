28f4e213ff39f40300def91b238bf29bcd32a65d
Naeem Khoshnevis <khoshnevis.naeem@gmail.com>
2015-02-09 19:41:44 -0600

    Coding to open the user defined file and read it. In this step I wrote the file content in output file, to make sure that reading and writing code work well

--- naeem.cpp
+++ naeem.cpp
@@ -5,3 +5,68 @@
  * Written by : Naeem Khoshnevis (nkhshnvs@memphis.edu)
  * Feb 8, 2015 
  */
+
+/*
+ * This program is written at Center for Earthquake Research 
+ * and Information (CERI) at University of Memphis. 
+ * All rights reserved. 
+ */
+
+ /* This script take a input file name from user and return
+  * outputfile and probabale error file.
+  * General idea of the function is to generate the summary report of the 
+  * seismic stations operations in California.
+  * In order to compile the code run make. Then you can run ()
+  * 
+  * Example of input file:
+  * 6 4
+  * CE.13882 CE.13162 CI.CHN CE.13068 CE.23511 CE.DLA
+  * Broadband StrongMotion ShortPeriod Borehole 
+  * CE.13882 StrongMotion 3
+  * CE.23511 Borehole 2
+  * CI.CHN ShortPeriod 1
+  * CE.13068 Broadband -2
+  * CE.23511 ShortPeriod 1
+  * CI.00000 HighGain 32
+  * 
+  */
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+string i;
+
+int main() {
+    
+    string   inputfilename;
+    ifstream inputfile;
+    ofstream outputfile;
+
+    // Prompt user for input file
+    cout << "Enter input filename: ";
+    cin  >> inputfilename;
+
+    // Sanity check on the file stream
+    inputfile.open(inputfilename.c_str());
+    
+    
+    if ( !inputfile.is_open() ) {
+
+        cout << "Cannot open input file: " 
+             << inputfilename
+             << endl;
+        return 1;
+    }
+
+    outputfile.open("naeem.out");
+    while ( inputfile >> i ) {
+
+    outputfile << i << "\n";
+
+    
+    }
+}
+
