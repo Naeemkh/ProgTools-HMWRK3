e67e9cdc6849026edf1b25754f9b8f1d30535587
Naeem Khoshnevis <khoshnevis.naeem@gmail.com>
2015-02-11 22:37:25 -0600

    up to this point The output file write the correct name and numbers but still it is not in format. Code is not giving warning

--- naeem.cpp
+++ naeem.cpp
@@ -46,10 +46,20 @@ char str1;
 string line;
 string st_name[100];
 string ins_name[10];
+string max_stations_record[100];
+string min_stations_record[100];
+string max_instrument_record[10];
+string min_instrument_record[10];
 
 string temp_station;
 string temp_instrument;
 int temp_event,temp_array,temp_array1, temp_ins_no;
+int max_n_station=0, min_n_station=0,max_n_instrument=0, min_n_instrument=0;
+
+int max_st=0;
+int min_st=0;
+int max_ins=0;
+int min_ins=0;
 
 int main() {
     
@@ -89,8 +99,10 @@ int main() {
     ins_no = i;
     
     int st_ins[st_no][ins_no];
-    int sum_stations[st_no][1];
-    int sum_instrument[ins_no][1];
+    int sum_stations[st_no];
+    int sum_instrument[ins_no];
+    
+
     
     
     for(i=0; i<st_no; i++)
@@ -110,27 +122,14 @@ int main() {
     
     
     
-    for(i=0; i<st_no; i++)
-        
-    {
-        
-        for(j=0; j<ins_no; j++)
-            
-        {
-            st_ins[i][j]=0;
-            
-            
-        }
-        
-    }
-    
+  
     
     
     
     for(i=0; i<st_no; i++)
         
     {
-       sum_stations[i][0]=0;
+       sum_stations[i]=0;
     
     }
     
@@ -138,7 +137,7 @@ int main() {
     for(i=0; i<st_no; i++)
         
     {
-        sum_instrument[i][0]=0;
+        sum_instrument[i]=0;
         
     }
     
@@ -177,7 +176,7 @@ int main() {
     outputfile  << "\n";
    
     
-    
+    int data_counter = 1;
     while (inputfile >> temp_station >> temp_instrument >> temp_event){
 
         
@@ -210,20 +209,26 @@ int main() {
        
        for (j=0; j < ins_no; j++){
            
-           sum_stations[i][0]+=   st_ins[i][j];
+           sum_stations[i]+=   st_ins[i][j];
        }
        
    }
+    
+    min_n_station = sum_stations[1];
+    max_n_station = sum_stations[1];
     
     for (j=0; j<ins_no; j++) {
         
         for (i=0; i < st_no; i++){
            
-            sum_instrument[j][0]+= st_ins[i][j];
+            sum_instrument[j]+= st_ins[i][j];
         }
         
     }
     
+    
+    min_n_instrument = sum_instrument [1];
+    max_n_instrument  = sum_instrument [1];
     
     for (k=0; k<st_no; k++) {
     
@@ -240,23 +245,188 @@ int main() {
         
     }
     
-    cout << "Stations :" << "\n";
+     outputfile << "\n";
+     outputfile << "Total number of events per station"<< "\n";
+     outputfile << "\n";
+  
     
-    for (i=0; i<st_no; i++) {
+    for (k=0; k<st_no; k++) {
         
         
-           cout<< sum_stations[i][0] << "\n";
+        outputfile <<  st_name[k] <<"\t" << sum_stations[k] << "\n";
+        
         
     }
     
-    cout << "instruments :" << "\n";
     
-    for (i=0; i<ins_no; i++) {
+    outputfile << "\n";
+    outputfile << "Total number of events per insrumetn"<< "\n";
+    outputfile << "\n";
+    
+    
+    for (k=0; k<ins_no; k++) {
         
         
-        cout<< sum_instrument[i][0] << "\t";
+        outputfile <<  ins_name[k] <<"\t" << sum_stations[k]<< "\n";
+        
         
     }
+    
+    // Calculation the most record for stations
+    
+    for (i=0;  i < st_no; i++) {
+        if (sum_stations[i] > max_n_station){
+            max_n_station = sum_stations[i];
+            cout << "if is true" << i << "\n";
+        }
+        cout << "for works i: " << i << "\n";
+    }
+    
+    // Calculation the least record for stations
+    
+    for (i=0; i < st_no; i++) {
+        if (sum_stations[i] < min_n_station){
+            min_n_station = sum_stations[i];
+        }
+        
+    }
+    
+     // Calculation the most record for instruments
+    for (i=0; i < ins_no; i++) {
+        if (sum_instrument[i] > max_n_instrument){
+            max_n_instrument = sum_instrument[i];
+        }
+        
+    }
+    
+    // Calculation the least record for instruments
+    for (i=0; i < ins_no ; i++) {
+        if (sum_instrument[i] < min_n_instrument){
+            min_n_instrument = sum_instrument[i];
+        }
+        
+    }
+    
+   
+    
+    for (i=0; i < st_no ; i++) {
+        if (sum_stations[i] == max_n_station){
+            max_stations_record[max_st] = st_name[i];
+            max_st=max_st+1;
+        }
+        
+    }
+    
+  
+    
+    for (i=0; i < st_no ; i++) {
+        if (sum_stations[i] == min_n_station){
+            min_stations_record[min_st] = st_name[i];
+            min_st=min_st+1;
+        }
+        
+    }
+
+   
+    
+    for (i=0; i < ins_no ; i++) {
+        if (sum_instrument[i] == max_n_instrument){
+            max_instrument_record[max_ins] = ins_name[i];
+            max_ins=max_ins+1;
+        }
+        
+    }
+    
+  
+    
+    for (i=0; i < ins_no ; i++) {
+        if (sum_instrument[i] == min_n_instrument){
+            min_instrument_record[min_ins] = ins_name[i];
+            min_ins=min_ins+1;
+        }
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    outputfile << "Stations with the  most records is: " ;
+    
+    for (i=0; i < max_st ; i++) {
+        if (max_st==1 || i == max_st-1) {
+            outputfile << max_stations_record[i];
+        } else  {
+            outputfile << max_stations_record[i] << ", ";
+         
+        }
+    }
+    
+    outputfile << " with " << max_n_station << " events" <<"\n" ;
+    
+    
+    
+    
+    outputfile << "Stations with the least records is: " ;
+    
+    for (i=0; i < min_st ; i++) {
+        if (min_st==1 || i == min_st-1) {
+            outputfile << min_stations_record[i];
+        } else  {
+            outputfile << min_stations_record[i] << ", ";
+            
+        }
+    }
+    
+    outputfile << " with " << min_n_station << " events" <<"\n"<<"\n" ;
+    
+    
+    
+    
+    
+    
+    outputfile << "Instruments with the  most records is: " ;
+    
+    for (i=0; i < max_ins ; i++) {
+        if (max_ins==1 || i == max_ins-1) {
+            outputfile << max_instrument_record[i];
+        } else  {
+            outputfile << max_instrument_record[i] << ", ";
+            
+        }
+    }
+    
+    outputfile << " with " << max_n_instrument << " events" <<"\n" ;
+
+    
+    
+    outputfile << "Instruments with the least records is: " ;
+    
+    for (i=0; i < min_ins ; i++) {
+        if (min_ins==1 || i == min_ins-1) {
+            outputfile << min_instrument_record[i];
+        } else  {
+            outputfile << min_instrument_record[i] << ", ";
+            
+        }
+    }
+    
+    outputfile << " with " << min_n_instrument << " events" <<"\n"<<"\n" ;
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    cout << "Min station : " << min_n_station <<"Min instrument:"<< min_n_instrument << "max station : " << max_n_station <<  "max instrument:" << max_n_instrument << "\n";
+    
     
     // Closing files
     inputfile.close();
