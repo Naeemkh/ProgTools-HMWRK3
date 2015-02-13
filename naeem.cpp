463a9beed0b46b6fd161b1cd1fb9ffa34cf7c99e
Naeem Khoshnevis <khoshnevis.naeem@gmail.com>
2015-02-12 20:49:34 -0600

    Programming is done. Styling is left, make file is left.

--- naeem.cpp
+++ naeem.cpp
@@ -35,15 +35,32 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <sstream>
-
+#include <iomanip>
 
 using namespace std;
 
-//string i;
-int st_no,ins_no,k,i,f,j,ev_number,f_index,s_index;
-char str1;
-string line;
+int i, j, k;
+int st_no, ins_no, ev_number, f_index, s_index, temp_event;
+
+int max_n_station = 0,
+    min_n_station = 0,
+    max_n_instrument= 0,
+    min_n_instrument= 0;
+
+int max_st  = 0,
+    min_st  = 0,
+    max_ins = 0,
+    min_ins = 0;
+
+int nvalid_ev  = 0,
+    nvalid_ins = 0,
+    nvalid_st  = 0;
+
+bool is_station,
+     is_instrument,
+     is_event;
+
+
 string st_name[100];
 string ins_name[10];
 string max_stations_record[100];
@@ -53,23 +70,13 @@ string min_instrument_record[10];
 
 string temp_station;
 string temp_instrument;
-int temp_event,temp_array,temp_array1, temp_ins_no;
-int max_n_station=0, min_n_station=0,max_n_instrument=0, min_n_instrument=0;
 
-int max_st=0;
-int min_st=0;
-int max_ins=0;
-int min_ins=0;
-
-int nvalid_ev=0,nvalid_ins=0,nvalid_st=0;
-
-bool is_station,is_instrument,is_event;
 
 int main() {
     
     string   inputfilename;
     ifstream inputfile;
-    ofstream outputfile;
+    ofstream outputfile,errorfile;
 
     // Prompt user for input file
     cout << "Enter input filename: ";
@@ -88,78 +95,57 @@ int main() {
     }
 
     outputfile.open("naeem.out");
+    errorfile.open("naeem.err");
     
     // Write the summary report in the output file
     outputfile << "Seismic Events Summary Report" << "\n"<<"\n";
     
     
-    
     inputfile >> i;
-    
     st_no = i;
     
     inputfile >> i;
-    
     ins_no = i;
     
     // number of stations
     cout << "\n";
     cout << "Number of stations: " << st_no << "\n";
     cout << "Number of instruments: " << ins_no << "\n";
-
     
-    
-    
+    errorfile << "\n";
+    errorfile << "Number of stations: " << st_no << "\n";
+    errorfile << "Number of instruments: " << ins_no << "\n";
     
     int st_ins[st_no][ins_no];
     int sum_stations[st_no];
     int sum_instrument[ins_no];
     
-
     
-    
-    for(i=0; i<st_no; i++)
+    for(i=0; i<st_no; i++){
         
-    {
-        
-        for(j=0; j<ins_no; j++)
+        for(j=0; j<ins_no; j++){
             
-        {
-            st_ins[i][j]=0;
-            
+            st_ins[i][j] = 0;
             
         }
+    }
+    
+    
+    for(i=0; i<st_no; i++){
+        
+       sum_stations[i] = 0;
         
     }
     
     
-    
-    
-  
-    
-    
-    
-    for(i=0; i<st_no; i++)
+    for(i=0; i<st_no; i++){
         
-    {
-       sum_stations[i]=0;
-    
-    }
-    
-    
-    for(i=0; i<st_no; i++)
-        
-    {
-        sum_instrument[i]=0;
+        sum_instrument[i] = 0;
         
     }
     
     
-
-    
-    
-   
-    
+ 
     for (j=0; j<st_no ; j++) {
         
         inputfile >> st_name[j];
@@ -175,14 +161,11 @@ int main() {
     
     
     // Write the summary report in the output file
-    outputfile << "Station" << " " ;
-    
-    
-
+    outputfile <<setw(10)<<left<< "Station" << " " ;
     
     for (i=0; i<st_no; i++) {
         
-         outputfile  << ins_name[i] << " " ;
+         outputfile  << setw(15) << right << ins_name[i] << " " ;
         
    }
   
@@ -190,11 +173,12 @@ int main() {
    
     
     int data_counter = 1;
+    
     while (inputfile >> temp_station >> temp_instrument >> temp_event){
         
-        is_station=false;
-        is_instrument=false;
-        is_event = false;
+        is_station    = false;
+        is_instrument = false;
+        is_event      = false;
         
         for (i=0; i < st_no; i++) {
             
@@ -220,51 +204,61 @@ int main() {
         }
         
         if (is_event && is_instrument && is_station){
-            
-                st_ins[f_index][s_index]=st_ins[f_index][s_index] + temp_event;
-        }else if(is_station==false){
+             st_ins[f_index][s_index]=st_ins[f_index][s_index] + temp_event;
+        } else if (is_station==false){
             cout << "Entry #"<<data_counter<<":"<<temp_station<<" "<<temp_instrument<<" "<<temp_event<<" "
             << "-- Warning: Station does not exist"<<"\n";
+            
+            errorfile << "Entry #"<<data_counter<<":"<<temp_station<<" "<<temp_instrument<<" "<<temp_event<<" "
+            << "-- Warning: Station does not exist"<<"\n";
+            
             nvalid_st=nvalid_st+1;
             
+        } else if (is_instrument==false){
+            cout << "Entry #"<<data_counter<<":"<<temp_station<<" "<<temp_instrument<<" "<<temp_event<<" "
+            << "-- Warning: Instrument does not exist"<<"\n";
+            
+            errorfile << "Entry #"<<data_counter<<":"<<temp_station<<" "<<temp_instrument<<" "<<temp_event<<" "
+            << "-- Warning: Instrument does not exist"<<"\n";
+            
+            
+            nvalid_ins=nvalid_ins+1;
+            
+        } else if (is_event==false){
+            cout << "Entry #"<<data_counter<<":"<<temp_station<<" "<<temp_instrument<<" "<<temp_event<<" "
+            << "-- Warning: Negative events"<<"\n";
+            
+            errorfile << "Entry #"<<data_counter<<":"<<temp_station<<" "<<temp_instrument<<" "<<temp_event<<" "
+            << "-- Warning: Negative events"<<"\n";
+            
+            nvalid_ev=nvalid_ev+1;
+                
         }
-            else if(is_instrument==false){
-                cout << "Entry #"<<data_counter<<":"<<temp_station<<" "<<temp_instrument<<" "<<temp_event<<" "
-                << "-- Warning: Instrument does not exist"<<"\n";
-                nvalid_ins=nvalid_ins+1;
                 
-            
-            }else if (is_event==false){
-                cout << "Entry #"<<data_counter<<":"<<temp_station<<" "<<temp_instrument<<" "<<temp_event<<" "
-                << "-- Warning: Negative events"<<"\n";
-                nvalid_ev=nvalid_ev+1;
-                
-            }
-                
-            
-        
-        
-        
-        
         
         data_counter=data_counter+1;
     }
     
     
-    cout << "Total of " << (data_counter-1)-(nvalid_st+nvalid_ins+nvalid_ev) << " entries processed correcty" <<"\n";
+    cout << "Total of " << (data_counter-1)-(nvalid_st+nvalid_ins+nvalid_ev) << " entries processed correctly" <<"\n";
     cout << "Total of " << (nvalid_st+nvalid_ins+nvalid_ev) << " ignored" <<"\n";
     cout << "Generating report... " << "\n";
     
+    
+    errorfile << "Total of " << (data_counter-1)-(nvalid_st+nvalid_ins+nvalid_ev) << " entries processed correctly" <<"\n";
+    errorfile << "Total of " << (nvalid_st+nvalid_ins+nvalid_ev) << " ignored" <<"\n";
+    errorfile << "Generating report... " << "\n";
+    
     // calculation of total number of event per station
     
-   for (i=0; i<st_no; i++) {
+    for (i=0; i<st_no; i++) {
        
        for (j=0; j < ins_no; j++){
            
            sum_stations[i]+=   st_ins[i][j];
        }
        
-   }
+    }
     
     min_n_station = sum_stations[1];
     max_n_station = sum_stations[1];
@@ -280,16 +274,16 @@ int main() {
     
     
     min_n_instrument = sum_instrument [1];
-    max_n_instrument  = sum_instrument [1];
+    max_n_instrument = sum_instrument [1];
     
     for (k=0; k<st_no; k++) {
     
         
-        outputfile <<  st_name[k] <<"\t";
+        outputfile <<setw(10)<<left<<  st_name[k] <<" ";
         
         for(j=0; j<ins_no; j++){
             
-        outputfile << st_ins[k][j] << "\t";
+        outputfile <<setw(15)<<right<< st_ins[k][j] << " ";
             
         }
          outputfile << "\n";
@@ -302,10 +296,12 @@ int main() {
      outputfile << "\n";
   
     
+     outputfile <<setw(10)<<left<<"Station"<<" "<<setw(15)<<right<<"Total"<<"\n";
+    
     for (k=0; k<st_no; k++) {
         
         
-        outputfile <<  st_name[k] <<"\t" << sum_stations[k] << "\n";
+        outputfile <<setw(10)<<left<<  st_name[k] << " "<<setw(15) <<right<< sum_stations[k] << "\n";
         
         
     }
@@ -316,10 +312,12 @@ int main() {
     outputfile << "\n";
     
     
+    
+     outputfile <<setw(15)<<left<<"Instrument"<<" "<<setw(10)<<right<<"Total"<<"\n";
     for (k=0; k<ins_no; k++) {
         
-        
-        outputfile <<  ins_name[k] <<"\t" << sum_stations[k]<< "\n";
+       
+        outputfile <<setw(15)<<left<< ins_name[k] <<" "<<setw(10)<<right << sum_instrument[k]<< "\n";
         
         
     }
@@ -400,11 +398,6 @@ int main() {
     }
 
     
-    
-    
-    
-    
-    
     outputfile << "Stations with the  most records is: " ;
     
     for (i=0; i < max_st ; i++) {
@@ -435,10 +428,6 @@ int main() {
     outputfile << " with " << min_n_station << " events" <<"\n"<<"\n" ;
     
     
-    
-    
-    
-    
     outputfile << "Instruments with the  most records is: " ;
     
     for (i=0; i < max_ins ; i++) {
@@ -453,7 +442,6 @@ int main() {
     outputfile << " with " << max_n_instrument << " events" <<"\n" ;
 
     
-    
     outputfile << "Instruments with the least records is: " ;
     
     for (i=0; i < min_ins ; i++) {
@@ -467,22 +455,15 @@ int main() {
     
     outputfile << " with " << min_n_instrument << " events" <<"\n"<<"\n" ;
     
-
+    cout << "Completed!" <<"\n"<<"\n";
     
-    
-    cout << "Completed!" <<"\n";
-    
-    
-    
-    
-    
-    
-   
+    errorfile << "Completed!" <<"\n"<<"\n";
     
     
     // Closing files
     inputfile.close();
     outputfile.close();
+    errorfile.close();
     
     return 0;
     
